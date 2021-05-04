5f8bbc2225e76fd0cd178d7a335fd16beb169a3b
Ivan MSI <arigatory@gmail.com>
2021-05-04 08:53:01 +0300

    fin

--- CalculateSumMASM/CalculateSumMASM.cpp
+++ CalculateSumMASM/CalculateSumMASM.cpp
@@ -1,20 +1,26 @@
-// CalculateSumMASM.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+
+extern "C" int AdderASM(int a, int b, int c);
+int AdderCPP(int a, int b, int c);
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a = 10, b = 20, c = 30;
+    int sum = AdderASM(a, b, c);
+
+    printf("a: %d\n", a);
+    printf("b: %d\n", b);
+    printf("c: %d\n", c);
+
+    printf("sum: %d\n", sum);
+
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int AdderCPP(int a, int b, int c)
+{
+    return a + b + c;
+}
